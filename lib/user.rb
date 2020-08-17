9281193eed1dc6eb2a0cb9afaff408381074fc50
Nathan Steed <steedn@outlook.com>
2020-08-17 03:43:06 +0000

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -1,7 +1,9 @@
 class User
-  attr_accessor :first_name, :last_name
-  def initialize(first, last)
-    @first_name = first
-    @last_name = last
+  attr_accessor :knowledge, :first_name, :last_name
+  def initialize
+    @knowledge = []
+  end
+  def learn(string)
+    self.knowledge << string
   end
 end
